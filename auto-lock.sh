dc153aab8cd337e0885a9e9d1d32ef0ad190c81a
yusanshi <meet.leiyu@gmail.com>
2022-07-19 21:55:40 +0800

    Update

--- auto-lock.sh
+++ auto-lock.sh
@@ -3,11 +3,26 @@
 # kill other instances of this script
 pgrep "$(basename $0)" | grep -v "$$" | xargs --no-run-if-empty kill 
 
+INDICATOR_TARGET=$HOME/.config/argos/auto-lock-indicator.1s.sh
+
 while true
 do
-    interval=$((${1:-50} * 60))
-    echo "Sleep $interval seconds"
-    sleep $interval
+    duration=$((${1:-50} * 60))
+    echo "Sleep $duration seconds"
+
+    start=$(date +%s)
+    remained_string='$(($start+$duration-$(date +%s)))'
+
+    cat <<EOF > $INDICATOR_TARGET
+#!/usr/bin/env bash
+
+start=$start
+duration=$duration
+
+echo "$remained_string seconds | iconName=system-lock-screen"
+EOF
+
+    sleep $duration
     for i in {1..200}
         do
             echo "Locking"
